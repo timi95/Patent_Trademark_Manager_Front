06441e8c2be8e5149c242673caaaff362988e18c
timi95 <timi.ogunkeye@gmail.com>
2020-06-05 17:24:52 +0100

    routes added
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './detail/detail.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'detail/:id', component: DetailComponent },
+  { path:'', component: MainContentComponent },
+  { path:'main-content', component: MainContentComponent }
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
