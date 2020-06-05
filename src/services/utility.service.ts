fd96eae75784da0bfbc39cbe02e916d17b395fa8
timi95 <timi.ogunkeye@gmail.com>
2020-06-05 17:24:11 +0100

    detail passing
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/services/utility.service.ts
+++ src/services/utility.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Subject, BehaviorSubject } from 'rxjs';
+import { AmendmentAction } from 'src/interfaces/AmendmentAction';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilityService {
   modalFormActive:BehaviorSubject<boolean>;
-
+  detailSubject: Subject<AmendmentAction>;
+  
   constructor() { 
     this.modalFormActive = new BehaviorSubject<boolean>(false);
+    this.detailSubject = new Subject<AmendmentAction>();
   }
-
-
+  
+  
   setModalFormActive(){
     return this.modalFormActive.next(true);
   }
   setModalFormInactive(){
     return this.modalFormActive.next(false);
+  }
+  loadDetails(item:AmendmentAction) {
+    this.detailSubject.next(item);
   }
 }
