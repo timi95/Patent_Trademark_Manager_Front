3855d16486845ca8731fb7b0b0b6e6ea2746b61d
timi95 <timi.ogunkeye@gmail.com>
2020-08-17 19:18:44 +0100

    remove cluttering log
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/app/pipes/pure-call.pipe.ts
+++ src/app/pipes/pure-call.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PureCallPipe implements PipeTransform {
 
   transform(func: Function, ...args: any[]): unknown {
-    console.log('Custom Pipe pureCall, called!');
     return func(...args);
   }
 
