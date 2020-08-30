bfb6bb03e038baa20465164200823a858a821e5d
timi95 <timi.ogunkeye@gmail.com>
2020-08-30 23:06:56 +0100

    adding type lists to side-nav components
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Action } from './classes/Action';
 
 @Component({
   selector: "app-root",
@@ -6,5 +7,8 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  patent_ActionTypes = new Action().patent_ActionTypes;
+  trademark_ActionTypes = new Action().trademark_ActionTypes;
+
   title = "Patent-Trademark-Manager-Front";
 }
