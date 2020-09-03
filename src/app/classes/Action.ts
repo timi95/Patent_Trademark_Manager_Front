615358d961fa0b7ac32ee85dd29008d077dd1fda
timi95 <timi.ogunkeye@gmail.com>
2020-09-03 22:49:13 +0100

    ascending order of alphabet
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/app/classes/Action.ts
+++ src/app/classes/Action.ts
@@ -7,40 +7,40 @@ export class Action {
 
       // list of Action-types, Particulars, and Profile
   public patent_ActionTypes:string[] = [
-    'SearchAction',
-    'RenewalAction',
-    'RegistrationAction',
-    'ProcurementOfCertificateAction',
-    'CTCAction',
-    'ChangeOfNameAction',
-    'ChangeOfAddressAction',
+    'AmendmentAction',
     'AssignmentMergerAction',
-    'AmendmentAction'
+    'ChangeOfAddressAction',
+    'ChangeOfNameAction',
+    'CTCAction',
+    'ProcurementOfCertificateAction',
+    'RegistrationAction',
+    'RenewalAction',
+    'SearchAction',
   ];
   public patent_particulars:string[] = ['PatentParticlars'];
   public trademark_ActionTypes:string[] = [
-    'SearchAction',
-    'RenewalAction',
-    'RegistrationAction',
-    'ReclassificationAction',
-    'ChangeName_AddressAction',
-    'CertificateProcurementAction',
+    'AmendementAction',
     'AssignmentMergerAction',
-    'AmendementAction'
+    'CertificateProcurementAction',
+    'ChangeName_AddressAction',
+    'ReclassificationAction',
+    'RegistrationAction',
+    'RenewalAction',
+    'SearchAction',
   ];
   public trademark_profile:string[] = ['TrademarkProfile'];
   // dictionaries
   public patentActionUrlDict = {
-    'SearchAction':'search-action',
-    'RenewalAction':'renewal-action',
-    'RegistrationAction':'registration',
-    'PatentParticulars':'patent-particulars',
-    'ProcurementOfCertificateAction':'procurement',
-    'CTCAction':'ctc',
-    'ChangeOfNameAction':'change-name',
-    'ChangeOfAddressAction':'change-address',
+    'AmendmentAction':"amendement-action",
     'AssignmentMergerAction':'assignment-merger-action',
-    'AmendmentAction':"amendement-action"
+    'ChangeOfAddressAction':'change-address',
+    'ChangeOfNameAction':'change-name',
+    'CTCAction':'ctc',
+    'ProcurementOfCertificateAction':'procurement',
+    'PatentParticulars':'patent-particulars',
+    'RegistrationAction':'registration',
+    'RenewalAction':'renewal-action',
+    'SearchAction':'search-action',
   }
   public trademarkActionUrlDict = {
     '':''
