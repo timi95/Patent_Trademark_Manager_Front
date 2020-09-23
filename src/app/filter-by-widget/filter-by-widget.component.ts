6599d70346d5911a8e57460ca057cdedd8c5ee5b
timi95 <timi.ogunkeye@gmail.com>
2020-09-23 21:32:11 +0100

    filter-by-widget implemented
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/app/filter-by-widget/filter-by-widget.component.ts
+++ src/app/filter-by-widget/filter-by-widget.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TestObject } from 'protractor/built/driverProviders';
+import { ApiService } from 'src/services/api.service';
+import { UtilityService } from 'src/services/utility.service';
+import { Action } from '../classes/Action';
 
 @Component({
   selector: 'filter-by',
@@ -6,14 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-by-widget.component.css']
 })
 export class FilterByWidgetComponent implements OnInit {
+  
+  patentActionUrlDict: any = new Action().patentActionUrlDict;
 
-  constructor() { }
+  documents = [];
+  orderTypeList:string[] = [];
+  documentTypeUrl: string;
+  // chosenOrderType:string = '';
+
+  constructor( private router: Router,
+    private utilityService: UtilityService,
+    private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.documents = JSON.parse( localStorage.getItem('documentList') );
+    this.orderTypeList = Object.keys(JSON.parse( localStorage.getItem('documentList'))[0]);
+    this.documentTypeUrl = this.patentActionUrlDict[localStorage.getItem('documentType')];
   }
 
-  sort(){
+  sort($event){
     console.log("sort by component call");
-    
+    this.apiService.orderPatentDocumentsByField(
+      this.documentTypeUrl, 
+      $event.target.value)
+    .subscribe(resp =>{
+      console.log('orderPatentDocumentsByField() called! response ==>', resp['results']);
+      localStorage.setItem('documentList', JSON.stringify(resp['results']) );
+      this.utilityService.updateDocumentList();
+    });
   }
 }
