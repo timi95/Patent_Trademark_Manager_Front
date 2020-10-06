adf1425d30b9ddbd1d9440949fb6906d6c82fab1
timi95 <timi.ogunkeye@gmail.com>
2020-10-06 20:40:04 +0100

    styling up, and setting up reminders
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/app/reminder-list/reminder-list.component.ts
+++ src/app/reminder-list/reminder-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-reminder-list',
+  selector: 'reminders',
   templateUrl: './reminder-list.component.html',
   styleUrls: ['./reminder-list.component.css']
 })
 export class ReminderListComponent implements OnInit {
-
+  reminders:any[] = [
+    {title:'Reminder 1', details:' Detailed instructions'},
+    {title:'Reminder 2', details:' Detailed instructions'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
