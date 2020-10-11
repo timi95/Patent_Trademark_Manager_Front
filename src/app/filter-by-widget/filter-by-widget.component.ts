d5d7c6b82517707ab0ebf3bdfda48d3d6bed1aaf
timi95 <timi.ogunkeye@gmail.com>
2020-10-11 14:53:49 +0100

    filter-by fix
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/app/filter-by-widget/filter-by-widget.component.ts
+++ src/app/filter-by-widget/filter-by-widget.component.ts
@@ -13,27 +13,34 @@ import { Action } from '../classes/Action';
 export class FilterByWidgetComponent implements OnInit {
   
   patentActionUrlDict: any = new Action().patentActionUrlDict;
+  trademarkActionUrlDict: any = new Action().trademarkActionUrlDict;
 
   documents = [];
   orderTypeList:string[] = [];
   documentTypeUrl: string;
+  managerType:string = 'Patent_manager';
   // chosenOrderType:string = '';
 
   constructor(private utilityService: UtilityService, private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.utilityService.updateManagerType();
+    this.utilityService.managerTypeSubject.subscribe(resp=>{this.managerType = resp});
     this.utilityService.updateDocumentList();
     this.utilityService.documentListSubject.subscribe(resp=>{this.orderTypeList=resp[0]?Object.keys(resp[0]):[];});
 
     this.utilityService.updateDocumentType();
-    this.utilityService.documentTypeSubject.subscribe(resp=>{this.documentTypeUrl=this.patentActionUrlDict[resp]});
+    this.utilityService.documentTypeSubject.subscribe(resp=>{
+      this.documentTypeUrl = (this.managerType == 'Patent_manager')?
+      this.patentActionUrlDict[resp]: this.trademarkActionUrlDict[resp];});
   }
 
   sort($event){
     // console.log("sort by component call");
     this.apiService.orderPatentDocumentsByField(
       this.documentTypeUrl, 
-      $event.target.value)
+      $event.target.value,
+      this.managerType)
     .subscribe(resp =>{
       // console.log('orderPatentDocumentsByField() called! response ==>', resp['results']);
       localStorage.setItem('documentList', JSON.stringify(resp['results']) );
