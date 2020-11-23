aaa18e749d066c326b72eaef499f691fc1d4aebe
Timi <timi.ogunkeye@gmail.com>
2020-11-23 22:05:39 +0000

    view patents stub generated
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

---
+++ src/app/view-patents/view-patents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-patents',
+  templateUrl: './view-patents.component.html',
+  styleUrls: ['./view-patents.component.css']
+})
+export class ViewPatentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
