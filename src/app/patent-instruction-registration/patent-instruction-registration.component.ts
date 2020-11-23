ff15e73f46d8f1753e693a32865468aad58dd5f5
Timi <timi.ogunkeye@gmail.com>
2020-11-23 22:04:21 +0000

    patent instruction registration stub generated
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

---
+++ src/app/patent-instruction-registration/patent-instruction-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patent-instruction-registration',
+  templateUrl: './patent-instruction-registration.component.html',
+  styleUrls: ['./patent-instruction-registration.component.css']
+})
+export class PatentInstructionRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
