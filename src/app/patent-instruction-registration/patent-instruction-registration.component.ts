83521e027fb53fabef022fc68095d84a95b62445
Timi <timi.ogunkeye@gmail.com>
2020-12-09 18:04:56 +0000

    patentCreateForm
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- src/app/patent-instruction-registration/patent-instruction-registration.component.ts
+++ src/app/patent-instruction-registration/patent-instruction-registration.component.ts
@@ -38,11 +38,45 @@ export class PatentInstructionRegistrationComponent implements OnInit {
     'assignment-merger-action':this.Forms.P_assignment_merger_action,
     "amendment-action": this.Forms.P_amendment_action,
   }
+  patentCreateForm = {
+    action_list: [],
+    applicable_service_charge: {value:"default value", type:"text"},
+    certificate_procurement_due_date: { value: new Date(), type: "date"},
+    clerk_responsible: {value:"default value", type:"text"},
+    client_id: {value:"default value", type:"text"},
+    clients_address: {value:"default value", type:"text"},
+    clients_contact_person: {value:"default value", type:"text"},
+    clients_reference_number: {value:"default value", type:"text"},
+    convention_country: {value:"default value", type:"text"},
+    current_instruction: {value:"default value", type:"text"},
+    current_status: {value:"default value", type:"text"},
+    date_completed_job_received: { value: new Date(), type: "date"},
+    date_incoming_abuja_schedule: { value: new Date(), type: "date"},
+    date_instruction_received: { value: new Date(), type: "date"},
+    date_of_instruction: { value: new Date(), type: "date"},
+    date_outgoing_abuja_schedule: { value: new Date(), type: "date"},
+    facilitation: {value:"default value", type:"text"},
+    filing_receipt_status: {value:"default value", type:"text"},
+    incentive_due_clerk: {value:"default value", type:"text"},
+    invention_description: {value:"default value", type:"text"},
+    lawyer_responsible: {value:"default value", type:"text"},
+    month_certificate_procurement_due: {value:"default value", type:"text"},
+    month_incoming_abuja_schedule: {value:"default value", type:"text"},
+    month_outgoing_abuja_schedule: {value:"default value", type:"text"},
+    name_of_client: {value:"default value", type:"text"},
+    name_of_patentee: {value:"default value", type:"text"},
+    official_fee: {value:"default value", type:"text"},
+    our_reference_number: {value:"default value", type:"text"},
+    patent_registration_number: {value:"default value", type:"text"},
+    patentee_address: {value:"default value", type:"text"},
+    quickteller_fee: {value:"default value", type:"text"},
+    type_id: "patent"
+  }
   constructor(public router: Router,
     public utilityService:UtilityService) { }
 
   ngOnInit(): void {
-    this.patent_particulars = this.utilityService.toList(this.Forms.P_patent_particulars);
+    // this.patent_particulars = this.utilityService.toList(this.Forms.patentCreateForm);
    }
  
    changeInstructionForm(event:any){
