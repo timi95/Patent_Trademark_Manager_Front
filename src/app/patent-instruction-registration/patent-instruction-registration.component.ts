d88286b9252978db3e7a2a97ea21107a1c030597
Timi <timi.ogunkeye@gmail.com>
2020-12-10 18:45:47 +0000

    get createForms from Form class
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- src/app/patent-instruction-registration/patent-instruction-registration.component.ts
+++ src/app/patent-instruction-registration/patent-instruction-registration.component.ts
@@ -37,40 +37,7 @@ export class PatentInstructionRegistrationComponent implements OnInit {
     'assignment-merger-action':this.Forms.P_assignment_merger_action,
     "amendment-action": this.Forms.P_amendment_action,
   }
-  patentCreateForm = {
-    action_list: {value:[], type:"list"},
-    applicable_service_charge: {value:"default value", type:"text"},
-    certificate_procurement_due_date: { value: new Date(), type: "datetime-local"},
-    clerk_responsible: {value:"default value", type:"text"},
-    client_id: {value:"default value", type:"text"},
-    clients_address: {value:"default value", type:"text"},
-    clients_contact_person: {value:"default value", type:"text"},
-    clients_reference_number: {value:"default value", type:"text"},
-    convention_country: {value:"default value", type:"text"},
-    current_instruction: {value:"default value", type:"text"},
-    current_status: {value:"default value", type:"text"},
-    date_completed_job_received: { value: new Date(), type: "datetime-local"},
-    date_incoming_abuja_schedule: { value: new Date(), type: "datetime-local"},
-    date_instruction_received: { value: new Date(), type: "datetime-local"},
-    date_of_instruction: { value: new Date(), type: "datetime-local"},
-    date_outgoing_abuja_schedule: { value: new Date(), type: "datetime-local"},
-    facilitation: {value:"default value", type:"text"},
-    filing_receipt_status: {value:"default value", type:"text"},
-    incentive_due_clerk: {value:"default value", type:"text"},
-    invention_description: {value:"default value", type:"text"},
-    lawyer_responsible: {value:"default value", type:"text"},
-    month_certificate_procurement_due: {value:"default value", type:"text"},
-    month_incoming_abuja_schedule: {value:"default value", type:"text"},
-    month_outgoing_abuja_schedule: {value:"default value", type:"text"},
-    name_of_client: {value:"default value", type:"text"},
-    name_of_patentee: {value:"default value", type:"text"},
-    official_fee: {value:"default value", type:"text"},
-    our_reference_number: {value:"default value", type:"text"},
-    patent_registration_number: {value:"default value", type:"text"},
-    patentee_address: {value:"default value", type:"text"},
-    quickteller_fee: {value:"default value", type:"text"},
-    type_id: {value:"patent", type:"type_id"},
-  }
+  patentCreateForm = this.Forms.patentCreateForm
   currentAction = {
     
   }
@@ -82,8 +49,7 @@ export class PatentInstructionRegistrationComponent implements OnInit {
    }
  
    changeInstructionForm(event:any){
-    console.log( this.utilityService
-      .toList(this.patentActionFormDictionary[event.target.value]) );
+    console.log( this.utilityService.toList(this.patentActionFormDictionary[event.target.value]));
     
     this.patentActionForm = this.patentActionFormDictionary[event.target.value]
     this.listOfPatentActionForm = this.utilityService.toList(this.patentActionForm);
