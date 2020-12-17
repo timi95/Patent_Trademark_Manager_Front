d3c15c5f4465ac8a3ca57ed4c7a475a61c5a2792
Timi <timi.ogunkeye@gmail.com>
2020-12-17 16:27:24 +0000

    get id from activated route on init
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UtilityService } from 'src/services/utility.service';
 import { AmendmentAction } from 'src/interfaces/AmendmentAction';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ApiService } from 'src/services/api.service';
 import { Action } from '../classes/Action';
+import { Patent } from '../classes/Instructions/Patent';
 
 @Component({
   selector: 'app-detail',
@@ -11,13 +12,26 @@ import { Action } from '../classes/Action';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit, OnDestroy {
-
-  constructor(private router:Router) {
+  patentID: string;
+  patent$: Patent;
+  constructor(
+    private router:Router,
+    private activatedRoute: ActivatedRoute,
+    public apiService: ApiService) {
   }
 
 
   ngOnInit(): void {
-    
+    this.activatedRoute.params.subscribe(
+      (params: Params) => {
+        console.log(params['id']);
+        this.patentID = params['id'];
+      }
+    );
+    this.apiService.documentRequest("patent","get", this.patentID)
+    .subscribe( (patent: Patent) => {
+      this.patent$ = patent;
+    });
   }
 
   ngOnDestroy(){
