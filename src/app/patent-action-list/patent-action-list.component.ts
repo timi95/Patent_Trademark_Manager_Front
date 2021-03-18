f91b4fc67385ad9c0a59346eacb64752a083b516
Timi <timi.ogunkeye@gmail.com>
2021-03-18 20:43:10 +0000

    get actionList from Action class
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- src/app/patent-action-list/patent-action-list.component.ts
+++ src/app/patent-action-list/patent-action-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { UtilityService } from 'src/services/utility.service';
+import { Action } from '../classes/Action';
 import { Form } from '../classes/Form';
 
 @Component({
@@ -13,27 +14,17 @@ export class PatentActionListComponent implements OnInit {
   Forms:Form = new Form();
   patentActionForm:any;
   listOfPatentActionForm:any[];
-  actionList = [
-    'search',
-    'renewal',
-    'registration',
-    'procurement',
-    'ctc',
-    'change-name',
-    'change-address',
-    'assignment-merger',
-    "amendment",
-  ];
+  actionList = new Action().patent_actionList
   patentActionFormDictionary =  {
-    'search':this.Forms.P_searchActionCreateForm,
-    'renewal':this.Forms.P_renewalActionCreateForm,
+    'search-action':this.Forms.P_searchActionCreateForm,
+    'renewal-action':this.Forms.P_renewalActionCreateForm,
     'registration':this.Forms.P_registrationActionCreateForm,
     'procurement':this.Forms.P_procurementOfCertificateActionCreateForm,
     'ctc':this.Forms.P_CTCActionCreateForm,
     'change-name':this.Forms.P_changeOfNameActionCreateForm,
     'change-address': this.Forms.P_changeOfAddressActionCreateForm,
-    'assignment-merger':this.Forms.P_assignmentMergerActionCreateForm,
-    "amendment": this.Forms.P_assignmentMergerActionCreateForm,
+    'assignment-merger-action':this.Forms.P_assignmentMergerActionCreateForm,
+    'amendment-action': this.Forms.P_assignmentMergerActionCreateForm,
   }
   patentCreateForm = this.Forms.patentCreateForm
   current_action = new Subject();
