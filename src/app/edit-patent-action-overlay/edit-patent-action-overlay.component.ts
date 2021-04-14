39f8608c22df118b6ba1eb2b4eb876c4290c5a71
Timi <timi.ogunkeye@gmail.com>
2021-04-14 22:51:07 +0100

    overlay inputs
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- src/app/edit-patent-action-overlay/edit-patent-action-overlay.component.ts
+++ src/app/edit-patent-action-overlay/edit-patent-action-overlay.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-patent-action-overlay',
+  selector: 'edit-p-action-overlay',
   templateUrl: './edit-patent-action-overlay.component.html',
   styleUrls: ['./edit-patent-action-overlay.component.css']
 })
 export class EditPatentActionOverlayComponent implements OnInit {
-
+  @Input('active') active: boolean;
+  @Input('targetId') targetId: string;
   constructor() { }
 
   ngOnInit(): void {
