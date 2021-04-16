415346781bbfaae913a2bb4e6981594a3ff0db6c
Timi <timi.ogunkeye@gmail.com>
2021-04-16 20:19:19 +0100

    stubbing stuff
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- src/app/edit-patent-action-overlay/edit-patent-action-overlay.component.ts
+++ src/app/edit-patent-action-overlay/edit-patent-action-overlay.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from 'src/services/api.service';
+import { UtilityService } from 'src/services/utility.service';
+import { Action } from '../interfaces/Action';
 
 @Component({
   selector: 'edit-p-action-overlay',
@@ -7,11 +10,19 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class EditPatentActionOverlayComponent implements OnInit {
   @Input('active') active: boolean;
-  @Input('targetId') targetId: string;
-  constructor() { }
+  @Input('targetId') targetActionId: string;
 
+  targetAction: Action;
+
+  constructor(private apiService: ApiService,
+              private utilityService: UtilityService) { }
 
   ngOnInit(): void {
+    this.apiService.documentRequest('action','get', this.targetActionId)
+    .subscribe((actionResponse:Action)=>{
+      console.log('resp', actionResponse);
+      this.targetAction = actionResponse;
+    });
   }
 
 }
