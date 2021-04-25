d5a2997b6c30612b0c3f471d07f3fe689b6b8fc3
Timi <timi.ogunkeye@gmail.com>
2021-04-25 16:34:22 +0100

    summon action form, then apply action
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- src/app/patent-action-list/patent-action-list.component.ts
+++ src/app/patent-action-list/patent-action-list.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { UtilityService } from 'src/services/utility.service';
 import { Action } from '../classes/Action';
+import * as IAction from '../interfaces/Action';
 import { Form } from '../classes/Form';
 import { Output, EventEmitter } from '@angular/core';
+import { ApiService } from 'src/services/api.service';
+import { MessageService } from 'src/services/message.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'action-list',
@@ -28,21 +32,72 @@ export class PatentActionListComponent implements OnInit {
     'amendment': this.Forms.P_assignmentMergerActionCreateForm,
   }
 
+  @Input('patentID') patentID: any;
   @Output() patentActionListData = new EventEmitter<PatentActionListComponentData>();
+  overlayActive: boolean = false;
+  targetAction: IAction.Action;
+  listOfTargetAction?: any[];
+  current_action = ''
+  is_editing: string = '';
+  // static methods
+  formMap = Form.formMap;
+  evaluateKey = Form.evaluateKey;
+  constructor(
+    private apiService: ApiService,
+    private messageService: MessageService,
+    public utilityService:UtilityService ) { }
 
+  changeActionForm(event:any){
+    this.current_action = event.target.value;
+    this.targetAction = this.formMap( this.patentActionFormDictionary[event.target.value] );
+    this.listOfTargetAction = Object.entries(this.targetAction);
+    this.overlayActive = true;
+    this.targetAction['type_id'] = this.current_action;
+    // this.patentActionListData.emit({
+    // current_action:event.target.value,
+    // patentActionForm: this.targetAction});
 
-
-  constructor( public utilityService:UtilityService ) { }
-
-  changeActionForm(event:any){    
-    this.patentActionListData.emit({
-    current_action:event.target.value,
-    patentActionForm:this.patentActionFormDictionary[event.target.value] });
-    
   }
 
 
   ngOnInit(): void {
+  }
+  shunOverlay($event){
+    if($event.target.classList.contains("overlay")){
+      this.setInactive();
+    }
+  }
+
+  setInactive(): void {
+    this.utilityService.setEditActionOverlayInactive();
+    this.utilityService.editActionOverlaySubject
+    .subscribe( bool => {this.overlayActive = bool;});
+  }
+  // Assigning a static method from the utility service class,
+  // allows it to be used in the template normally
+  stripFieldsFromList = UtilityService.stripFieldsFromList;
+  toggleEdit(prop:string){
+    this.is_editing = prop
+  }
+  cancelEdit(){
+    this.is_editing = ''
+  }
+  applyCurrentAction(){
+
+    // console.log(`/Instruction/patent/${this.patentID}/${this.current_action}`,
+    // "object:",this.targetAction);
+
+    this.apiService
+    .documentRequest(
+      'patent',
+      'put',
+      `${this.patentID}/${this.current_action}`,
+      JSON.stringify(this.targetAction))
+          .subscribe(resp=>{
+            this.messageService.pushSuccess(`Applied action: ${resp}`)},
+      (errorResponse:HttpErrorResponse)=>{
+        this.messageService.pushError(errorResponse.error)
+      });
   }
 
 }
