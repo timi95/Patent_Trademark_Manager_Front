b5574719d792ddc4dda0123df7b2617cabeebb9c
Timi <timi.ogunkeye@gmail.com>
2021-06-26 15:55:29 +0100

    .
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -109,15 +109,17 @@ public errors: any = [];
     imageData?: File,
     documentType?: string,
     imageName?: string) : Observable<any>{
-
+    const multiPart = new HttpHeaders().set('Content-Type', 'multipart/form-data');
     const formData = imageData? new FormData().append(imageData.name,imageData):null;
     let requestTypeDict = {
       "get":
       id? this.httpServer.get(`${this.API_URL}/Instruction/image/${id}`):
       imageName?this.httpServer.get(`${this.API_URL}/Instruction/image-name?name=${imageName}`, { responseType: 'blob' }):
       this.httpServer.get(`${this.API_URL}/Instruction/image/`),
+
       "post":
       this.httpServer.post(`${this.API_URL}/Instruction/${documentType}/${id}/image`, formData),
+      
       "delete":
       this.httpServer.delete(`${this.API_URL}/Instruction/image/${id}`),
     }
