6cf1487baf667e273f5d18de512d2d494e264673
Timi <timi.ogunkeye@gmail.com>
2021-08-30 17:10:50 +0100

    adding common modules to trademark module
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- src/app/trademark/trademark.module.ts
+++ src/app/trademark/trademark.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewTrademarksComponent } from './view-trademarks/view-trademarks.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ViewTrademarksComponent],
-  exports: [ViewTrademarksComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  exports: [ViewTrademarksComponent]
+
 })
 export class TrademarkModule { }
